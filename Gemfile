e3b11c25e2ae52770fb6f9b388a47180c1d1514f
craig wann and eric shull <craig@>
2018-09-17 11:09:40 -0700

    add seed file and html on products view

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.0'
-
+gem 'faker'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use postgresql as the database for Active Record
@@ -10,6 +11,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+# Order.destroy_all
+
+30.times do |index|
+  Product.create!(name: Faker::Beer.name,
+    price: Faker::Number.decimal(2, 2))
+end
+
+# 30.times do |index|
+#   Product.create!(name: Faker::Food.fruits,
+#     cost: Faker::Number.non_zero_digit,
+#     origin: "Mexico")
+# end
+#
+# Product.all.each do |product|
+#     60.times do |index|
+#       product.reviews.create!(author: Faker::Name.name, content: Faker::Lorem.sentence(20),
+#       rating: Faker::Number.between(1, 5),
+#       product_id: Product.all.shuffle[0].id)
+#   end
+# end
+
+p "Created #{Product.count} products"
