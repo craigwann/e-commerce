0c29ee3cafd20f1adc6117815b371bcbb19850af
craig wann and eric shull <eric@>
2018-09-17 15:55:44 -0700

    order order_item and product models and relationships

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
   }
   root :to => 'products#index'
-  resources :orders
+  resources :order_items
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
