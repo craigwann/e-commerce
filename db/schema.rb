3ec662893663ff81e222a4b45220a4e52ce9210d
craig wann and eric shull <eric@>
2018-09-18 08:41:57 -0700

    remove user id column from orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_210324) do
+ActiveRecord::Schema.define(version: 2018_09_18_153902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2018_09_17_210324) do
   create_table "orders", force: :cascade do |t|
     t.string "status"
     t.decimal "total_price", precision: 8, scale: 2
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
