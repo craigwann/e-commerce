4761cd0c09eeb5a1960c11ecce94d040de0cace7
craig wann and eric shull <craig@>
2018-09-19 11:36:34 -0700

    add ajax for items in cart on home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    @product = Product.first
     @products = Product.all
     @order_item = current_order.order_items.new
     # binding.pry
